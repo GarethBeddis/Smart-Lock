09cedf97894d32df6372976ba701cec105ed8ed6
Gareth Beddis <garethbeddis@gmail.com>
2020-03-17 14:17:08 +0000

    Add account linking for Google, Facebook and GitHub

--- my-smart-lock/src/app.js
+++ my-smart-lock/src/app.js
@@ -26,9 +26,32 @@ app.use(
 
 app.setHandler({
     LAUNCH() {
-        this.$speech.addText(`Welcome to your smart lock. Would you like to lock your door? Check the status of your door? Or unlock your door?`)
-        this.$reprompt.addText(`Do you want to get the status of your door lock, lock the door, or unlock the door?`)
-        this.ask(this.$speech, this.$reprompt);
+        // Check if customer is signed in
+        if(!this.$request.getAccessToken()){ // Customer not signed in
+            this.showAccountLinkingCard()
+            if(this.isAlexaSkill()){
+                this.$alexaSkill.tell('Please open the Alexa App to link your smart lock account.');
+            }
+        } else {
+            // Customer signed in!
+            this.$speech.addText(`Welcome to your smart lock. Would you like to lock your door? Check the status of your door? Or unlock your door?`)
+            this.$reprompt.addText(`Do you want to get the status of your door lock, lock the door, or unlock the door?`)
+            this.ask(this.$speech, this.$reprompt);
+        }
+    },
+
+    ON_SIGN_IN() {
+        if (this.$googleAction.getSignInStatus() === 'CANCELLED') {
+            // User did not link their account
+            this.showAccountLinkingCard();
+        } else if (this.$googleAction.getSignInStatus() === 'OK') {
+            // User linked their account
+            this.toIntent("LAUNCH");
+            this.tell(this.$speech);
+        } else if (this.$googleAction.getSignInStatus() === 'ERROR') {
+            // There was an error
+            this.showAccountLinkingCard()
+        }
     },
 
     LockIntent() {
